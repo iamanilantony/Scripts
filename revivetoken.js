85c091e4ab06b1b67734d2d18950b1ab2ab92596
Anil Antony <anilantony3134@gmail.com>
2023-09-28 17:26:33 +0530

    feat: Add visit script

---
+++ revivetoken.js
@@ -0,0 +1,372 @@
+const processInfo = {
+  process: "confirmation",
+  stage: "pending",
+  status: "mailSent",
+  updatedAt: ISODate("2022-10-17T10:53:57.380+0000"),
+  updatedBy: "oKCiKwBMTdBq6pNQS",
+  history: [
+    {
+      status: "pending",
+      createdAt: ISODate("2020-02-28T06:17:23.459+0000"),
+      createdBy: "Anil",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2020-02-28T06:17:40.251+0000"),
+      createdBy: "anilantony",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2020-02-28T06:18:17.608+0000"),
+      createdBy: "anil",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2020-02-28T06:26:21.451+0000"),
+      createdBy: "anil",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "pending",
+      incomplete: true,
+      createdAt: ISODate("2022-10-17T10:53:29.307+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2022-10-17T10:53:57.380+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+  ],
+  incomplete: true,
+};
+
+const userToken = {
+  unitId: "pAApES6EWdBrKrNT7",
+  token: "FMFL7Z8G6juKgfbwn",
+  device: {
+    type: "email",
+    id: "kadugodisanjay@gmail.com",
+  },
+  expiresAt: ISODate("2022-10-24T10:53:57.681+0000"),
+};
+
+const novisitProcessDetails = {
+  process: "verification",
+  stage: "pending",
+  status: "notRequiredReq",
+  updatedAt: ISODate("2022-10-17T10:56:47.586+0000"),
+  updatedBy: "oKCiKwBMTdBq6pNQS",
+  history: [
+    {
+      status: "pending",
+      createdAt: ISODate("2022-05-29T09:14:41.115+0000"),
+      createdBy: "8Z6CnYxsFfqsMnpFC",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+    },
+    {
+      status: "scheduled",
+      createdAt: ISODate("2022-10-11T22:36:53.915+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T00:30:00.000+0000"),
+      visitId: "baCsyz9CfwxjNDPgM",
+    },
+    {
+      status: "confirmed",
+      notes: "jbnmb",
+      createdAt: ISODate("2022-10-11T22:38:21.985+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T05:30:00.000+0000"),
+      visitExec: "QBoTdNAxGWYcMaJyP",
+      visitId: "baCsyz9CfwxjNDPgM",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-11T22:43:48.380+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T06:30:00.000+0000"),
+      visitExec: "QBoTdNAxGWYcMaJyP",
+      visitId: "baCsyz9CfwxjNDPgM",
+    },
+    {
+      status: "rescheduled",
+      createdAt: ISODate("2022-10-11T22:44:17.747+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T04:30:00.000+0000"),
+      visitId: "w8q2Z2efmLGWnvbWv",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-11T22:45:21.296+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T04:30:00.000+0000"),
+      visitExec: "NoMj7ShwwPWbGr7dQ",
+      visitId: "w8q2Z2efmLGWnvbWv",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-11T22:45:45.908+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T05:30:00.000+0000"),
+      visitExec: "QBoTdNAxGWYcMaJyP",
+      visitId: "w8q2Z2efmLGWnvbWv",
+    },
+    {
+      status: "cancelled",
+      notes: "hgjhjh",
+      createdAt: ISODate("2022-10-17T10:56:22.688+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T05:30:00.000+0000"),
+      visitId: "w8q2Z2efmLGWnvbWv",
+    },
+    {
+      status: "notRequiredReq",
+      createdAt: ISODate("2022-10-17T10:56:47.586+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "8Z6CnYxsFfqsMnpFC",
+      appointmentDate: ISODate("2022-10-12T05:30:00.000+0000"),
+      reason: "owner",
+    },
+  ],
+  appointmentDate: ISODate("2022-10-12T05:30:00.000+0000"),
+};
+
+const visitNotRequiredToken = {
+  unitId: "wxYkHyuRejNugK4p4",
+  token: "QSW7piXKiNmWLYC2u",
+  device: {
+    type: "email",
+    id: "in.kunaal@gmail.com",
+  },
+  expiresAt: ISODate("2022-10-24T10:56:47.745+0000"),
+};
+
+const agreementToken = {
+  groupId: "5pmNG4S9Mwi2QjKs9",
+  token: "gje3ZHHpxg9XnWij4",
+  device: {
+    type: "email",
+    id: "akumar229@gmail.com",
+  },
+  expiresAt: ISODate("2022-10-24T10:59:19.946+0000"),
+};
+
+const agreementProcessDetails = {
+  process: "agreement",
+  status: "draftSent",
+  stage: "pending",
+  updatedBy: "SHb7WxD38R72ZMscZ",
+  updatedAt: ISODate("2022-10-17T10:59:38.669+0000"),
+};
+
+const confirmFeeeProcessDetails = {
+  process: "confirmation",
+  stage: "pending",
+  status: "pending",
+  updatedAt: ISODate("2022-10-18T10:14:40.012+0000"),
+  updatedBy: "PWq7ACA6HJFFDtybm",
+  history: [
+    {
+      status: "pending",
+      createdAt: ISODate("2020-02-28T06:17:23.459+0000"),
+      createdBy: "Anil",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2020-02-28T06:17:40.251+0000"),
+      createdBy: "anilantony",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2020-02-28T06:18:17.608+0000"),
+      createdBy: "anil",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2020-02-28T06:26:21.451+0000"),
+      createdBy: "anil",
+      assignedTo: "JSMYfw5CJorBEuBDg",
+    },
+    {
+      status: "pending",
+      incomplete: true,
+      createdAt: ISODate("2022-10-17T10:53:29.307+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2022-10-17T10:53:57.380+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "mailSent",
+      createdAt: ISODate("2022-10-17T13:59:38.290+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-17T15:22:23.786+0000"),
+      createdBy: "7d4Ja5QC68EWKFrvR",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "pending",
+      incomplete: true,
+      createdAt: ISODate("2022-10-17T15:29:38.914+0000"),
+      createdBy: "oKCiKwBMTdBq6pNQS",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-17T15:30:53.324+0000"),
+      createdBy: "7d4Ja5QC68EWKFrvR",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-18T08:35:56.412+0000"),
+      createdBy: "7d4Ja5QC68EWKFrvR",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "pending",
+      incomplete: true,
+      createdAt: ISODate("2022-10-18T10:05:28.258+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "QbDWrSZkpDFhN8uh3",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-18T10:09:52.273+0000"),
+      createdBy: "7d4Ja5QC68EWKFrvR",
+      assignedTo: "WLRK6GbRoFKWQywHJ",
+    },
+    {
+      status: "pending",
+      incomplete: true,
+      createdAt: ISODate("2022-10-18T10:11:48.567+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "WLRK6GbRoFKWQywHJ",
+    },
+    {
+      status: "confirmed",
+      createdAt: ISODate("2022-10-18T10:13:36.448+0000"),
+      createdBy: "7d4Ja5QC68EWKFrvR",
+      assignedTo: "WLRK6GbRoFKWQywHJ",
+    },
+    {
+      status: "pending",
+      incomplete: true,
+      createdAt: ISODate("2022-10-18T10:14:40.012+0000"),
+      createdBy: "PWq7ACA6HJFFDtybm",
+      assignedTo: "WLRK6GbRoFKWQywHJ",
+    },
+  ],
+  incomplete: true,
+};
+
+const confirmFeeSuDetails = {
+  process: "confirmation",
+  stage: "pending",
+  status: "pending",
+  updatedAt: ISODate("2022-10-18T10:11:47.976+0000"),
+  updatedBy: "PWq7ACA6HJFFDtybm",
+  incomplete: true,
+};
+
+const confirmfeeToken = {
+  unitId: "pAApES6EWdBrKrNT7",
+  token: "nZsbf8h336gFHqZgu",
+  device: {
+    type: "email",
+    id: "kadugodisanjay@gmail.com",
+  },
+  expiresAt: ISODate("2022-10-25T10:15:26.591+0000"),
+};
+
+function visitNotRequired() {
+  db.units.find({ _id: "wxYkHyuRejNugK4p4" }).forEach((u) => {
+    u.processDetails = u.processDetails || [];
+    u.processDetails[2] = novisitProcessDetails;
+    print(u._id, "kll");
+    db.units.update(
+      { _id: u._id },
+      { $set: { processDetails: u.processDetails } }
+    );
+  });
+
+  db.users.find({ _id: "RfMMcLxQN7ksRDdqZ" }).forEach((u) => {
+    // u.authTokens = u.authTokens || {};
+    u.authTokens.visitNotRequiredConfirmation.push(visitNotRequiredToken);
+    print(u._id, "klasal");
+    db.users.update({ _id: u._id }, { $set: { authTokens: u.authTokens } });
+  });
+}
+
+function agreement() {
+  db.groups.find({ _id: "5pmNG4S9Mwi2QjKs9" }).forEach((u) => {
+    u.processDetails[1] = agreementProcessDetails;
+    print(u._id, "kll");
+    db.units.update(
+      { _id: u._id },
+      { $set: { processDetails: u.processDetails } }
+    );
+  });
+  print("Agreement Revived");
+
+  db.users.find({ _id: "4P4eqYCwoCz5TKDFL" }).forEach((u) => {
+    // u.authTokens = u.authTokens || {};
+    u.authTokens.buyerAgreementConfirmation.push(agreementToken);
+    print(u._id, "klasal");
+    db.users.update({ _id: u._id }, { $set: { authTokens: u.authTokens } });
+  });
+}
+
+function feeConfirmation() {
+  db.units.find({ _id: "pAApES6EWdBrKrNT7" }).forEach((u) => {
+    u.processDetails = u.processDetails || [];
+    u.processDetails[1] = confirmFeeeProcessDetails;
+    print(u._id, "kll");
+    db.units.update(
+      { _id: u._id },
+      { $set: { processDetails: u.processDetails } }
+    );
+  });
+
+  db.users.find({ _id: "7d4Ja5QC68EWKFrvR" }).forEach((u) => {
+    u.authTokens = u.authTokens || {};
+    u.authTokens.unitConfirmation.push(confirmfeeToken);
+    print(u._id, "klasal");
+    db.users.update({ _id: u._id }, { $set: { authTokens: u.authTokens } });
+  });
+
+  db.sellerUnits.find({ _id: "dqHMh3xC4mbrGjC9x" }).forEach((u) => {
+    u.processDetails[1] = confirmFeeSuDetails;
+    db.sellerUnits.update(
+      { _id: u._id },
+      { $set: { processDetails: u.processDetails } }
+    );
+  });
+}
+
+// visitNotRequired()
+feeConfirmation();
+// agreement()
